c3674e712f4e24947eb911794acb3b595a2b2b56
asterixchan <90444357+asterixchan@users.noreply.github.com>
2021-09-16 22:39:18 +0700

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-const close = document.querySelectorAll(".close");
-// const card = document.querySelector(".card");
-
-close.forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    e.target.parentElement.style.display = "none";
-    e.preventDefault("blank");
-  });
-});
